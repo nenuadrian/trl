57cb9551a17688d26d6ba39772aa806f02afdbec
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2026-01-28 11:54:24 +0000

    vmpo-initial

--- trl/experimental/vmpo/__init__.py
+++ trl/experimental/vmpo/__init__.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .vmpo_config import VMPOConfig
-from .vmpo_trainer import VMPOTrainer
+from .vmpo_trainer import VMPOTrainer, VMPOConfig
 
 
 __all__ = [
