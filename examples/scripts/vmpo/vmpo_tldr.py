f248c43506acfbc19bec9045e2c062acaa93aecc
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2026-01-30 13:08:05 +0000

    vmpo-tldr

--- examples/scripts/vmpo/vmpo_tldr.py
+++ examples/scripts/vmpo/vmpo_tldr.py
@@ -34,50 +34,12 @@ from transformers import (
     HfArgumentParser,
 )
 
-from trl import (
-    ModelConfig,
-    ScriptArguments,
-    get_kbit_device_map,
-    get_peft_config,
-    get_quantization_config,
-)
+from trl import ModelConfig, ScriptArguments, get_kbit_device_map, get_peft_config, get_quantization_config
 from trl.experimental.vmpo import VMPOConfig, VMPOTrainer
 
 
 # Enable logging in a Hugging Face Space
 os.environ.setdefault("TRACKIO_SPACE_ID", "trl-trackio")
-
-
-"""
-python -i examples/scripts/vmpo.py \
-    --dataset_name trl-internal-testing/descriptiveness-sentiment-trl-style \
-    --dataset_train_split descriptiveness \
-    --learning_rate 3e-6 \
-    --output_dir pythia-1b-deduped-descriptiveness-sentiment-trl-style-vmpo \
-    --per_device_train_batch_size 64 \
-    --gradient_accumulation_steps 1 \
-    --total_episodes 10000 \
-    --model_name_or_path EleutherAI/pythia-1b-deduped \
-    --missing_eos_penalty 1.0
-
-accelerate launch --config_file examples/accelerate_configs/deepspeed_zero3.yaml \
-    examples/scripts/vmpo.py \
-    --dataset_name trl-internal-testing/descriptiveness-sentiment-trl-style \
-    --dataset_train_split descriptiveness \
-    --output_dir pythia-1b-deduped-descriptiveness-sentiment-trl-style-vmpo \
-    --num_vmpo_epochs 1 \
-    --num_mini_batches 1 \
-    --learning_rate 3e-6 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 16 \
-    --total_episodes 10000 \
-    --model_name_or_path EleutherAI/pythia-1b-deduped \
-    --sft_model_path EleutherAI/pythia-1b-deduped \
-    --reward_model_path EleutherAI/pythia-1b-deduped \
-    --local_rollout_forward_batch_size 1 \
-    --missing_eos_penalty 1.0
-"""
-
 
 if __name__ == "__main__":
     parser = HfArgumentParser((ScriptArguments, VMPOConfig, ModelConfig))
@@ -88,11 +50,7 @@ if __name__ == "__main__":
     ################
     # Model & Tokenizer
     ################
-    dtype = (
-        model_args.dtype
-        if model_args.dtype in ["auto", None]
-        else getattr(torch, model_args.dtype)
-    )
+    dtype = model_args.dtype if model_args.dtype in ["auto", None] else getattr(torch, model_args.dtype)
     model_kwargs = dict(
         revision=model_args.model_revision,
         attn_implementation=model_args.attn_implementation,
@@ -105,9 +63,7 @@ if __name__ == "__main__":
         model_kwargs["quantization_config"] = quantization_config
 
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path,
-        padding_side="left",
-        trust_remote_code=model_args.trust_remote_code,
+        model_args.model_name_or_path, padding_side="left", trust_remote_code=model_args.trust_remote_code
     )
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
     value_model = AutoModelForSequenceClassification.from_pretrained(
@@ -123,17 +79,13 @@ if __name__ == "__main__":
         **model_kwargs,
     )
     policy = AutoModelForCausalLM.from_pretrained(
-        training_args.sft_model_path,
-        trust_remote_code=model_args.trust_remote_code,
-        **model_kwargs,
+        training_args.sft_model_path, trust_remote_code=model_args.trust_remote_code, **model_kwargs
     )
 
     peft_config = get_peft_config(model_args)
     if peft_config is None:
         ref_policy = AutoModelForCausalLM.from_pretrained(
-            training_args.sft_model_path,
-            trust_remote_code=model_args.trust_remote_code,
-            **model_kwargs,
+            training_args.sft_model_path, trust_remote_code=model_args.trust_remote_code, **model_kwargs
         )
     else:
         ref_policy = None
@@ -141,29 +93,19 @@ if __name__ == "__main__":
     ################
     # Dataset
     ################
-    dataset = load_dataset(
-        script_args.dataset_name,
-        name=script_args.dataset_config,
-        split=script_args.dataset_train_split,
-    )
-    eval_samples = 100
-    train_dataset = dataset.select(range(len(dataset) - eval_samples))
-    eval_dataset = dataset.select(range(len(dataset) - eval_samples, len(dataset)))
-    dataset_text_field = "prompt"
+    dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config)
+    train_dataset = dataset[script_args.dataset_train_split]
+    eval_dataset = dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None
 
     def prepare_dataset(dataset, tokenizer):
         """pre-tokenize the dataset before training; only collate during training"""
 
         def tokenize(element):
-            outputs = tokenizer(
-                element[dataset_text_field],
-                padding=False,
-            )
-            return {"input_ids": outputs["input_ids"]}
+            input_ids = tokenizer(element["prompt"], padding=False)["input_ids"]
+            return {"input_ids": input_ids, "lengths": len(input_ids)}
 
         return dataset.map(
             tokenize,
-            batched=True,
             remove_columns=dataset.column_names,
             num_proc=training_args.dataset_num_proc,
         )
@@ -172,7 +114,14 @@ if __name__ == "__main__":
     # see: https://github.com/huggingface/trl/pull/1255
     with PartialState().local_main_process_first():
         train_dataset = prepare_dataset(train_dataset, tokenizer)
-        eval_dataset = prepare_dataset(eval_dataset, tokenizer)
+        if eval_dataset is not None:
+            eval_dataset = prepare_dataset(eval_dataset, tokenizer)
+        # filtering
+        train_dataset = train_dataset.filter(lambda x: x["lengths"] <= 512, num_proc=training_args.dataset_num_proc)
+        if eval_dataset is not None:
+            eval_dataset = eval_dataset.filter(lambda x: x["lengths"] <= 512, num_proc=training_args.dataset_num_proc)
+
+    assert train_dataset[0]["input_ids"][-1] != tokenizer.eos_token_id, "The last token should not be an EOS token"
 
     ################
     # Training
